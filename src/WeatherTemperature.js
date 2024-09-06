5cb8e6a28a03a61023c75a4ee5d6913396883e0f
bethanchatwin <168184672+bethanchatwin@users.noreply.github.com>
2024-09-06 17:25:27 +0100

    added weather unit conversion function

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
-  return (
-    <div className="weather-app-tempertaure">
-      <div className="weather-app-temperature" id="current-temp">
-        {Math.round(props.data.temperature)}°C{" "}
+  const [unit, setUnit] = useState("celsius");
+  function showFarenheight(event) {
+    event.preventDefault();
+    setUnit("farenheight");
+  }
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+  if (unit === "celsius") {
+    return (
+      <div>
+        <span className="weather-app-temperature" id="current-temp">
+          {Math.round(props.celsius)}
+        </span>
+        <span className="weather-app-unit">
+          °C{" "}
+          <a href="/" onClick={showFarenheight}>
+            °F
+          </a>
+        </span>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let farenheight = (props.celsius * 9) / 5 + 32;
+    return (
+      <div>
+        <span className="weather-app-temperature" id="current-temp">
+          {Math.round(farenheight)}
+        </span>
+        <span className="weather-app-unit">
+          <a href="/" onClick={showCelsius}>
+            °C
+          </a>
+          °F
+        </span>
+      </div>
+    );
+  }
 }
